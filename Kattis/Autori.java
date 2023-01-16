5b8c38b879a8ae66a461fcb3aba1f3b7ce9e99ee
zhuming15 <e0735464@u.nus.edu>
2023-01-16 22:58:28 +0800

    Upload Autori CS2030 Version

--- Kattis/Autori.java
+++ Kattis/Autori.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 
 public class Autori {
-	public static String transform(String s) {
+	private final String s;
+	
+	Autori(String s) {
+		this.s = s;
+	}
+
+	private String getNextChar(int startIndex) {
+		return s.substring(startIndex + 1, startIndex + 2);
+	}
+
+	public String transform() {
 		int indexOfHyphen = s.indexOf("-");
 		String shortForm = (s.length() >= 0) ? (s.substring(0, 1)) : "";
 
-		if (indexOfHyphen == -1) {
-			return shortForm;
-		}
+		if (indexOfHyphen == -1) return shortForm;
 
 		while (indexOfHyphen != -1) {
-			shortForm += s.substring(indexOfHyphen + 1, indexOfHyphen + 2);
+			shortForm += this.getNextChar(indexOfHyphen);
 			indexOfHyphen = s.indexOf("-", indexOfHyphen + 1);
 		}
-
 		return shortForm;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		System.out.println("ShortForm is " + Autori.transform(s));
+
+		Autori test = new Autori(s);
+		System.out.println("ShortForm is " + test.transform());
 	}	
 }
