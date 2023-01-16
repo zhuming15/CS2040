3b3575bdab4befa02aa4de73d04b353248226a02
zhuming15 <e0735464@u.nus.edu>
2023-01-16 22:28:32 +0800

    Upload solution for Autori

---
+++ Kattis/Autori.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Autori {
+	public static String transform(String s) {
+		int indexOfHyphen = s.indexOf("-");
+		String shortForm = (s.length() >= 0) ? (s.substring(0, 1)) : "";
+
+		if (indexOfHyphen == -1) {
+			return shortForm;
+		}
+
+		while (indexOfHyphen != -1) {
+			shortForm += s.substring(indexOfHyphen + 1, indexOfHyphen + 2);
+			indexOfHyphen = s.indexOf("-", indexOfHyphen + 1);
+		}
+
+		return shortForm;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println("ShortForm is " + Autori.transform(s));
+	}	
+}
