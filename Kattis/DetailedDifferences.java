ee6c1c11c6e5523552b611f0f26f05f67b4ca35f
zhuming15 <e0735464@u.nus.edu>
2023-01-18 16:20:52 +0800

    Upload DetailedDifferences

---
+++ Kattis/DetailedDifferences.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DetailedDifferences {
+	public static void compareThenPrint(String s1, String s2) {
+		StringBuilder finalString = new StringBuilder();
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) == s2.charAt(i)) {
+				finalString.append(".");
+			} else {
+				finalString.append("*");
+			}
+		}
+		System.out.println(s1 + "\n" + s2 + "\n" + finalString + "\n");
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int counter = sc.nextInt();
+
+		while (counter > 0) {
+			String s1 = sc.next();
+			String s2 = sc.next();
+			DetailedDifferences.compareThenPrint(s1, s2);
+			counter--;
+		}
+	}
+}
