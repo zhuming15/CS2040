5b5d54569a9708bafa9ca6cd3c68f62bcefdb77c
zhuming15 <e0735464@u.nus.edu>
2023-01-18 14:29:27 +0800

    Upload TwoStones

--- Kattis/TwoStones.java
+++ Kattis/TwoStones.java
@@ -1,2 +1,16 @@
-class TwoStones { 
-	
+import java.util.Scanner;
+
+class TwoStones {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		if (n % 2 == 0) {
+			System.out.println("Alice");
+			return;
+		} else {
+			System.out.println("Bob");
+			return;
+		}
+	}
+}
