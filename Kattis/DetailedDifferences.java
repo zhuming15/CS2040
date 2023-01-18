d6bacef404130f462d69258b2d7f047bfcac0c2d
zhuming15 <e0735464@u.nus.edu>
2023-01-18 16:29:30 +0800

    Upload DetailedDifferences

--- Kattis/DetailedDifferences.java
+++ Kattis/DetailedDifferences.java
@@ -4,11 +4,10 @@ public class DetailedDifferences {
 	public static void compareThenPrint(String s1, String s2) {
 		StringBuilder finalString = new StringBuilder();
 		for (int i = 0; i < s1.length(); i++) {
-			if (s1.charAt(i) == s2.charAt(i)) {
+			if (s1.charAt(i) == s2.charAt(i)) 
 				finalString.append(".");
-			} else {
+		   	else 
 				finalString.append("*");
-			}
 		}
 		System.out.println(s1 + "\n" + s2 + "\n" + finalString + "\n");
 	}
