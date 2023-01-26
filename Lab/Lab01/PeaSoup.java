afb04954178c0800795517c25e1ddccd03b6a710
zhuming15 <e0735464@u.nus.edu>
2023-01-26 15:13:03 +0800

    Update Lab 01

--- Lab/Lab01/PeaSoup.java
+++ Lab/Lab01/PeaSoup.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PeaSoup {
-	public static void main(String[] args) {
+	public static void findVersionOne() {
 		Scanner sc = new Scanner(System.in);
 		int numOfRestaurant = sc.nextInt();
 
@@ -26,5 +26,35 @@ public class PeaSoup {
 			}
 		}
 		System.out.println("Anywhere is fine I guess");
+	}
+
+	public static void findVersionTwo() {
+		Scanner sc = new Scanner(System.in);
+		int numOfRestaurant = Integer.valueOf(sc.nextLine());
+
+		for (int i = 0; i < numOfRestaurant; i++) {
+			int numOfItems = Integer.valueOf(sc.nextLine());
+			String restaurantName = sc.nextLine();
+			boolean hasPancakes = false;
+			boolean hasPeaSoup = false;
+
+			for (int j = 0; j < numOfItems; j++) {
+				String dish = sc.nextLine();
+				if (dish.equals("pancakes"))
+					hasPancakes = true;
+				else if (dish.equals("pea soup"))
+					hasPeaSoup = true;
+
+				if (hasPeaSoup && hasPancakes) {
+					System.out.println(restaurantName);
+					return;
+				}
+			}
+		}
+		System.out.println("Anywhere is fine I guess");
+	}
+
+	public static void main(String[] args) {
+		PeaSoup.findVersionTwo();
 	}
 }
