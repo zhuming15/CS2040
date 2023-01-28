7cc01346338bd5a1814280966af3bec165d11960
zhuming15 <e0735464@u.nus.edu>
2023-01-29 03:03:10 +0800

    Modify Apaxians from O(n^2) to O(n)

--- Kattis/Apaxians/Apaxians.java
+++ Kattis/Apaxians/Apaxians.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.HashMap;
+
 
 public class Apaxians {
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder str = new StringBuilder(sc.nextLine());
 		int counter = 0;
@@ -13,6 +15,19 @@ public class Apaxians {
 				continue;
 			}
 			counter++;
+		}
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		StringBuilder str = new StringBuilder();
+		str.append(input.charAt(0));
+
+		for (int index = 1; index < input.length(); index++) {
+			if (input.charAt(index - 1) != input.charAt(index)) 
+				str.append(input.charAt(index));
 		}
 		System.out.println(str);
 	}
