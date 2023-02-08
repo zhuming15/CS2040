dc8d9a3112886cab05227b8a4681fd80d732b65a
zhuming15 <e0735464@u.nus.edu>
2023-02-08 18:08:31 +0800

    Modify ShatteredCake IO

--- Kattis/ShatteredCake/ShatteredCake.java
+++ Kattis/ShatteredCake/ShatteredCake.java
@@ -3,22 +3,17 @@ import java.io.*;
 public class ShatteredCake {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
 		int WIDTH = Integer.parseInt(br.readLine());
 		int numOfCakes = Integer.parseInt(br.readLine());
 		int totalArea = 0;
 
-		for (int nthCake = 0; nthCake < numOfCakes; nthCake++) {
+		while (numOfCakes-- > 0) {
 			String[] strarr = br.readLine().split(" ");
 			int width = Integer.parseInt(strarr[0]);
 			int length = Integer.parseInt(strarr[1]);
 			totalArea += width * length;
 		}
-
-		int LENGTH = totalArea / WIDTH;
-		pw.println(LENGTH);
-		pw.flush();
-		pw.close();
+		System.out.println(totalArea / WIDTH);
 	}
 }
