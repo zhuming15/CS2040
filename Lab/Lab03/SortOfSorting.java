cd50f720bae41f0e1b1f8e884dd8ac406832421f
zhuming15 <e0735464@u.nus.edu>
2023-02-09 17:15:37 +0800

    Modify SortOfSorting

--- Lab/Lab03/SortOfSorting.java
+++ Lab/Lab03/SortOfSorting.java
@@ -2,23 +2,35 @@ import java.io.*;
 import java.util.Arrays;
 
 class SortOfSorting {
-  public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-		int numOfNames = Integer.parseInt(br.readLine());
-		while (numOfNames != 0) {
-			String[] strarr = new String[numOfNames];
-			for (int i = 0; i < numOfNames; i++) {
-				strarr[i] = br.readLine();
-			}
-			Arrays.sort(strarr, (s1, s2) -> (s1.substring(0,2).compareTo(s2.substring(0,2))));
-			for (String s : strarr) {
-				pw.println(s);
-			}
-			pw.println("");
-			numOfNames = Integer.parseInt(br.readLine());
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String[] readInput( int numOfNames) throws IOException {
+		String[] strarr = new String[numOfNames];
+		for (int i = 0; i < numOfNames; i++) {
+			strarr[i] = SortOfSorting.br.readLine();
 		}
-		pw.flush();
-		pw.close();
+		return strarr;
+	}		
+
+	public static void sortThenPrint(String[] arr) {
+		Arrays.sort(arr, (s1, s2) -> (s1.substring(0,2).compareTo(s2.substring(0,2))));
+
+		for (String s: arr) {
+			SortOfSorting.pw.println(s);
+		}
+		SortOfSorting.pw.println("");
+	}
+
+	public static void main(String[] args) throws IOException {
+		SortOfSorting test = new SortOfSorting();
+		int numOfNames = Integer.parseInt(SortOfSorting.br.readLine());
+		while (numOfNames != 0) {
+			String[] strarr = SortOfSorting.readInput(numOfNames);
+			SortOfSorting.sortThenPrint(strarr);
+			numOfNames = Integer.parseInt(SortOfSorting.br.readLine());
+		}
+		SortOfSorting.pw.flush();
+		SortOfSorting.pw.close();
 	}
 }
