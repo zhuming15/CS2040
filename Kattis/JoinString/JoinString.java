d3337cc64c91761d3d266f63956e10c97f530999
zhuming15 <e0735464@u.nus.edu>
2023-02-14 16:33:15 +0800

    Upload JoinString

--- Kattis/JoinString/JoinString.java
+++ Kattis/JoinString/JoinString.java
@@ -5,29 +5,72 @@ import java.util.StringTokenizer;
 public class JoinString {
 	public static void main(String[] args) throws IOException {
 		Kattio io = new Kattio(System.in, System.out);
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
 		int numOfStrings = io.getInt();
-		StringBuilder[] wordList = new StringBuilder[numOfStrings+2];
+		StringLinkedList[] wordList = new StringLinkedList[numOfStrings+2];
+		StringLinkedList head = null;
 
 		for (int counter = 1; counter <= numOfStrings; counter++) {
-			wordList[counter] = new StringBuilder(io.getWord());
+			wordList[counter] = new StringLinkedList(io.getWord());
 		}
 
-		StringBuilder lastString = wordList[1] == null ? new StringBuilder() : wordList[1];
+		if (numOfStrings == 1) {
+		   	io.print(wordList[1].toString());
+			io.close();
+			return; 
+		}
+		if (numOfStrings == 0) {
+			io.print("");
+			io.close();
+			return;
+		}
 
 		for (int ithOperation = 1; ithOperation < numOfStrings; ithOperation++) {
 			int a = io.getInt();
 			int b = io.getInt();
-
-			wordList[a].append(wordList[b]);
-			lastString = wordList[a];
+			wordList[a].setNext(wordList[b]);
+			head = wordList[a];
 		}
-		pw.println(lastString.toString());
-		pw.flush();
-		pw.close();
+
+		while (head.hasNext()) {
+			io.print(head.toString());
+			head = head.getNext();
+		}
+		io.print(head.toString());
+		io.close();
 	}
 }	
+
+class StringLinkedList {
+	String string;
+	StringLinkedList next;
+
+	StringLinkedList(String s) {
+		this(s, null);
+	}
+	
+	StringLinkedList(String s, StringLinkedList next) {
+		this.string = s;
+		this.next = next;
+	}
+
+	boolean hasNext() {
+		return this.next != null;
+	}
+
+	void setNext(StringLinkedList next) {
+		this.next = next;
+	}
+
+	StringLinkedList getNext() {
+		return this.next;
+	}
+
+	@Override
+	public String toString() {
+		return this.string;
+	}
+}
 
 class Kattio extends PrintWriter {
 	public Kattio(InputStream i) {
