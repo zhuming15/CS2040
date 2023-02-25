1de15a37194cc02ba1d455b8fbf213c62f619623
zhuming15 <e0735464@u.nus.edu>
2023-02-25 17:17:43 +0800

    Upload HashMap version Teque

--- Kattis/Teque/Teque.java
+++ Kattis/Teque/Teque.java
@@ -1,49 +1,58 @@
 import java.util.*;
 
 class Teque {
-	public ListUsingArray frontArr = new ListUsingArray();
-	public ListUsingArray backArr = new ListUsingArray();
+	public PseudoArr frontArr = new PseudoArr();
+	public PseudoArr backArr = new PseudoArr();
 
-	public int size() {
-		return frontArr.size() + backArr.size();
-	}
+	public int size() { return frontArr.size() + backArr.size(); }
 
 	public void pushFront(int item) {
-		frontArr.addFront(item);
+		if (frontArr.size() > backArr.size()) {
+			int shiftItem = frontArr.removeBack();
+			backArr.pushFront(shiftItem);
+		}
+		frontArr.pushFront(item);
 	}
 
 	public void pushBack(int item) {
-		backArr.addBack(item);
+		if (backArr.size() > frontArr.size()) {
+			int shiftItem = backArr.removeFront();
+			frontArr.pushBack(shiftItem);
+		}
+		backArr.pushBack(item);
 	}
 
 	public void pushMiddle(int item) {
-		frontArr.addBack(item);
+		int insertionIndex = (this.size() + 1) / 2;
+		while (frontArr.size() != insertionIndex) {
+			frontArr.pushBack(backArr.removeFront());
+		}
+		backArr.pushFront(item);
 	}
 
 	public int get(int index) {
-		if (index < 0 || index > size()-1) {
-			System.out.println(index);
-			System.out.println("from Teque get");
-			System.out.println("invalid index");
-			System.exit(1);
+		if (index < frontArr.size()) {
+			return frontArr.get(index);
 		}
-		if (index < frontArr.size()) 
-			return frontArr.getItemAtIndex(index);
-		else
-			return backArr.getItemAtIndex(index-frontArr.size());
+		return backArr.get(index-frontArr.size());
+	}
+
+	public void print() {
+		System.out.println(frontArr.toString());
+		System.out.println(backArr.toString());
 	}
 
 	public static void main(String[] args) {
 		Kattio io = new Kattio(System.in, System.out);
 		Teque tq = new Teque();
 		int numOfOperations = io.getInt();
-		while (numOfOperations-- > 0) {
+		while(numOfOperations-- > 0) {
 			String command = io.getWord();
 			int input = io.getInt();
-			if (command.equals("push_back"))
-				tq.pushBack(input);
-			else if (command.equals("push_front"))
+			if (command.equals("push_front"))
 				tq.pushFront(input);
+			else if (command.equals("push_back"))
+				tq.pushBack(input);
 			else if (command.equals("push_middle"))
 				tq.pushMiddle(input);
 			else
