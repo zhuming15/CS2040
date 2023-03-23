e6573c265d3f4ba861b40e5f99b08e7c59df21e8
zhuming15 <e0735464@u.nus.edu>
2023-03-24 02:05:43 +0800

    Upload AlmostUnionFind

--- Kattis/AlmostUnionFind/AlmostUnionFind.java
+++ Kattis/AlmostUnionFind/AlmostUnionFind.java
@@ -1,4 +1,34 @@
 import java.util.*;
 
 class AlmostUnionFind {
-	public myUnionSet us;
+	public Set set;
+
+	AlmostUnionFind(int N) {
+		this.set = new Set(N);
+	}
+
+	void runOperation(int command, Kattio io) {
+		if (command == 1) { set.unionSet(io.getInt(), io.getInt()); }
+		else if (command == 2) { set.move(io.getInt(), io.getInt()); }
+		else { set.printOutput(io.getInt(), io); }
+	}
+	
+	void printSet(Kattio io) {
+		io.println(set.toString());
+	}
+
+	public static void main(String[] args) {
+		Kattio io = new Kattio(System.in, System.out);
+		while (io.hasMoreTokens()) {
+			int N = io.getInt();
+			int numOfOperations = io.getInt();
+			AlmostUnionFind test = new AlmostUnionFind(N);
+			
+			while (numOfOperations-- > 0) {
+				int operation = io.getInt();
+				test.runOperation(operation, io);
+			}
+		}
+		io.flush();
+	}
+}					
--- Kattis/AlmostUnionFind/Set.java
+++ Kattis/AlmostUnionFind/Set.java
@@ -2,80 +2,68 @@ import java.util.*;
 
 class Set {
 	public int[] root;
-	public int[] parent;
+	public int[] cont;
+	public int[] sum;
 	public int[] rank;
-	public int numOfElems;
 
 	Set(int N) {
-		this.numOfElems = N;
 		this.root = new int[N+1];
-		this.parent = new int [N+1];
-		this.rank = new int[N+1];
+		this.cont = new int [N+1];
+		this.sum = new int[N+1];
 		for (int i = 0; i < N + 1; i++) {
 			root[i] = i;
-			parent[i] = i;
-			rank[i] = 1;
-		}
-	}
-
-	int findRootSet(int i) {
-		int rootIdx = root[i];
-		if (rootIdx == i) { return i; }
-		else {
-			root[i] = rootIdx;
-			return findRootSet(rootIdx);
-		}
-	}
-
-	int findParentSet(int i) {
-		int parentIdx = parent[i];
-		if (parentIdx == i) { return i; }
-		else {
-			parent[i] = parentIdx;
-			return findParentSet(parentIdx);
+			cont[i] = 1;
+			sum[i] = i;
 		}
 	}
 
 	int findSet(int i) {
-		int rootIdx = findRootSet(i);
-		int parentIdx = findParentSet(rootIdx);
-		return parentIdx;
-	}
-
-	boolean isSameSet(int p, int q) {
-		return findSet(p) == findSet(q);
+		if (root[i] == i) { return i; }
+		else {
+			root[i] = findSet(root[i]);
+			return root[i];
+		}
 	}
 
 	void unionSet(int p, int q) {
-		if (!isSameSet(p, q)) {
-			int x = findSet(p);
-			int y = findSet(q);
-			if (rank[x]> rank[y]) {
-				root[y] = x;
-				parent[y] = x;
-			} else {
-				root[x] = y;
-				parent[p] = y;
-				if (rank[x] == rank[y]) {
-					rank[y] = rank[y] + 1;
-				}
-			}
+		int x = findSet(p);
+		int y = findSet(q);
+		if (x != y) {
+			cont[y] += cont[x];
+			sum[y] += sum[x];
+			root[x] = y;
 		}
+		findSet(p);
+		findSet(q);
 	}
 
 	void move(int p, int q) {
-		if(!isSameSet(p, q)) {
-			int qRootIdx = findRootSet(q);
-			int qParentIdx = findParentSet(qRootIdx);
-			root[p] = qParentIdx;
+		int x = findSet(p);
+		int y = findSet(q);
+		if (x != y) {
+			cont[x]--;
+			cont[y]++;
+			sum[x] -= p;
+			sum[y] += p;
+			root[p] = y;
 		}
+		findSet(p);
+		findSet(q);
+	}
+
+	void printOutput(int p, Kattio io) {
+		int x = findSet(p);
+		io.print(cont[x]);
+		io.print(" ");
+		io.print(sum[x]);
+		io.println("");
 	}
 
 	@Override
 	public String toString() {
-		System.out.println("parent : " + Arrays.toString(parent));
 		System.out.println("root   : " + Arrays.toString(root));
-		System.out.println("rank   : " + Arrays.toString(rank));
+		System.out.println("cont   : " + Arrays.toString(cont));
+		System.out.println("sum    : " + Arrays.toString(sum));
 		return "end";
 	}
 }	
